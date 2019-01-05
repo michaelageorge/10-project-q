52392eab422e2bba20320709c2045c5b6fcee20f
george <geomarray@gmail.com>
2019-01-04 16:59:00 -0800

    progress

---
+++ clients/logger.js
@@ -0,0 +1,13 @@
+const Q = require('../lib/subscriber.js');
+
+const db = new Q('database');
+
+db.subscribe('delete', (payload) => {
+  console.log('delete happened', payload);
+});
+
+db.subscribe('create', (payload) => {
+  console.log('create happened', payload);
+});
+
+console.log(db.subscriptions());
--- server.js
+++ server.js
@@ -1,1 +1,19 @@
 'use strict';
+const util = require('util');
+
+const Q = require('./lib/server-queue.js');
+Q.start();
+
+const db = new Q('database');
+db.connection()
+// db.doSomething();
+// console.log(db);
+// db.monitorEvent('create');
+// db.monitorEvent('update');
+// db.monitorEvent('delete');
+
+// const network = new Q('network');
+// network.monitorEvent('attack');
+// network.monitorEvent('no-service');
+
+// util.inspect(db, { showHidden: true, depth: null }
