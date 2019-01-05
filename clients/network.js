3fcbd936a2b62382971f20d4a767c0ae8fec3554
george <geomarray@gmail.com>
2019-01-04 19:18:33 -0800

    made connection

--- clients/network.js
+++ clients/network.js
@@ -1,6 +1,10 @@
-const Publisher = require('./lib/publisher.js');
-const Q = new Publisher();
+'use strict';
+// const Publisher = require('./lib/publisher.js');
+// const Q = new Publisher();
 
-Q.publish('database', 'delete', {id:77});
-Q.publish('database', 'create', {id:99,name:'John'});
-Q.publish('network', 'attack', {type: 'DDOS',source:'Russia'});
+// Q.publish('database', 'delete', {id:77});
+// Q.publish('database', 'create', {id:99,name:'John'});
+// Q.publish('network', 'attack', {type: 'DDOS',source:'Russia'});
+const io = require('socket.io-client');
+
+const something = io.connect('http://localhost:3003/database');
--- server.js
+++ server.js
@@ -1,19 +1,27 @@
 'use strict';
 const util = require('util');
+// const io = require('socket.io')(3003);
 
 const Q = require('./lib/server-queue.js');
+
 Q.start();
 
 const db = new Q('database');
-// db.connection()
+
+// io.on('connection', (socket)=>{
+//   console.log(socket.id);
+// });
+
+// db.connect();
 // db.doSomething();
 // console.log(db);
 // db.monitorEvent('create');
 // db.monitorEvent('update');
 // db.monitorEvent('delete');
 
-// const network = new Q('network');
+const network = new Q('network');
 // network.monitorEvent('attack');
 // network.monitorEvent('no-service');
 
+// network.connect();
 // util.inspect(db, { showHidden: true, depth: null }
