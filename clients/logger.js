fce8508619f5c5cbcfddc100f951a8bb407faa8a
hingham <hannah.c.ingham@gmail.com>
2019-01-05 17:33:44 -0800

    change during phone converstaion

--- clients/logger.js
+++ clients/logger.js
@@ -3,6 +3,8 @@ const Q = require('../lib/subscriber.js');
 const db = new Q('database');
 
 
+console.log('db', db);
+
 db.subscribe('delete', (payload) => {
   console.log('delete happened', payload);
 });
@@ -11,4 +13,8 @@ db.subscribe('create', (payload) => {
   console.log('create happened', payload);
 });
 
-console.log(db.subscriptions());
+db.subscribe('banana', (payload) =>{
+  console.log('banana happened', payload);
+});
+
+// console.log('subscriptions ', db.subscriptions);
