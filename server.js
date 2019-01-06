692d6c4b221a9fc2c9d36ef6481fb70706e38fc2
george <geomarray@gmail.com>
2019-01-05 16:19:32 -0800

    subscriber and publisher making connections to server

--- server.js
+++ server.js
@@ -1,27 +1,17 @@
 'use strict';
-const util = require('util');
-// const io = require('socket.io')(3003);
 
 const Q = require('./lib/server-queue.js');
+
 
 Q.start();
 
 const db = new Q('database');
-
-// io.on('connection', (socket)=>{
-//   console.log(socket.id);
-// });
-
-// db.connect();
-// db.doSomething();
-// console.log(db);
-// db.monitorEvent('create');
-// db.monitorEvent('update');
-// db.monitorEvent('delete');
+db.monitorEvent('create');
+db.monitorEvent('update');
+db.monitorEvent('delete');
+console.log(`database events: ${db.events}`);
 
 const network = new Q('network');
-// network.monitorEvent('attack');
-// network.monitorEvent('no-service');
-
-// network.connect();
-// util.inspect(db, { showHidden: true, depth: null }
+network.monitorEvent('attack');
+network.monitorEvent('no-service');
+console.log(`network events: ${network.events}`);
