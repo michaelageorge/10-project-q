bc4380d451bb0673fcf067f1c16d8f01c7c6bf05
Michael George <mgeorge9492@gmail.com>
2019-01-06 23:15:39 -0800

    best shot at test for Subscriber, README updated... confused about a couple of categories in the README

--- __tests__/subscriber.test.js
+++ __tests__/subscriber.test.js
@@ -1,3 +1,12 @@
 'use strict';
 
-const subscriber = require('../lib/subscriber');
+const Subscriber = require('../lib/subscriber.js');
+
+describe('subsciber', () => {
+  test('emits subscriber events', () => {
+    const subscriber = new Subscriber('database');
+    let spy = jest.spyOn(subscriber, 'subscribe');
+    subscriber.subscribe();
+    expect(spy).toHaveBeenCalled();
+  });
+});
