8fdb29c443d7a65250368cf5c038cc20482300b9
hingham <hannah.c.ingham@gmail.com>
2019-01-06 21:54:27 -0800

    tryinig to write tests for publish

--- __tests__/publisher.test.js
+++ __tests__/publisher.test.js
@@ -1,3 +1,18 @@
 'use strict';
 
 const publisher = require('../lib/publisher');
+
+describe('testing jest' , () => {
+
+  test('see if console.log was called', () => {
+    const q = new publisher();
+    const spy = jest.spyOn(console, 'log');
+    q.publish('database', 'delete', {id:77});
+    expect(spy).toHaveBeenCalled();
+    spy.mockRestore();
+  });
+  test('if new object is created', () => {
+    const q = new publisher();
+    expect(typeof(q)).toBe('object');
+  });
+});
